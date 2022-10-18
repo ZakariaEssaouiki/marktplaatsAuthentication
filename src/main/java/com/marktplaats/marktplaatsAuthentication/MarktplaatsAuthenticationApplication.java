1ce90a20aaf2eb364ef3f957ec7ac44e48349941
Zakaria <z.essaouiki@ziggo.nl>
2022-10-18 10:23:13 +0200

    spring boot applicatie gemaakt

---
+++ src/main/java/com/marktplaats/marktplaatsAuthentication/MarktplaatsAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.marktplaats.marktplaatsAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarktplaatsAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarktplaatsAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marktplaats/marktplaatsAuthentication/MarktplaatsAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marktplaats.marktplaatsAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarktplaatsAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
